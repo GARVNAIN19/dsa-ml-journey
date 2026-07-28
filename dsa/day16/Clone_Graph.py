947cf05414bf4faada2bfc87922f3a5b56e5c676
GARVNAIN19 <Garvnain87@gmail.com>
2026-07-28 21:07:58 +0530

    Day 16 - Clone Graph (LeetCode 133)

---
+++ dsa/day16/Clone_Graph.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def cloneGraph(self, node):
+        if not node:
+            return None
+
+        visited = {}  
+
+        def dfs(n):
+            if n in visited:
+                return visited[n]  
+
+            clone = Node(n.val)   
+            visited[n] = clone    
+
+            for neighbor in n.neighbors:
+                clone.neighbors.append(dfs(neighbor))  
+
+            return clone
+
+        return dfs(node)
